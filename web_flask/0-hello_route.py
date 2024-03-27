660da1189b90a32b91e6cfbbca59a5e4dc7f295d
yaniG <yane12girma@gmail.com>
2024-03-27 03:15:39 -0400

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+start Flask application
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
