5b32e1cfbad59c954d689a5fbbf1837483c9e74c
yaniG <yane12girma@gmail.com>
2024-03-27 03:08:25 -0400

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""start Flask application."""
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
