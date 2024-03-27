680611272c33f98ad7ff6a8c9ce3c7ba68f44d00
yaniG <yane12girma@gmail.com>
2024-03-27 03:48:17 -0400

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,6 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
 
-            pass
             
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
