5679220b4d0fa658c1ef7b3dfcdeca332768b54a
yaniG <yane12girma@gmail.com>
2024-03-27 03:16:42 -0400

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+start Flask application
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return 'HBNB'
 
 if __name__ == '__main__':
